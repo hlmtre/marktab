a8f7f4cd1c51b7b73f9cf930a44da76b1bdabc3e
Chris Knadler <takeshi91k@gmail.com>
2013-01-06 22:56:27 -0800

    Adds starting point for hash

---
+++ src/mt_hash.c
@@ -0,0 +1,3 @@
+#include "mt_hash.h"
+
+#include <assert.h>
---
+++ src/mt_hash.h
@@ -0,0 +1,27 @@
+#ifndef MT_HASH_H
+#define MT_HASH_H
+
+#include <stdbool.h>
+
+#include "mt_string.h"
+
+typedef struct MtHash
+{
+  struct MtHashElement* buckets;
+  size_t size;
+  size_t length;
+
+} MtHash;
+
+MtHash* mt_hash_new();
+
+void mt_hash_insert(MtHash* hash, MtString* key, void* value);
+
+bool mt_hash_contains_key(MtHash* hash, MtString* key);
+
+void* mt_hash_search(MtHash* hash, MtString* key);
+
+void mt_hash_clear(MtHash* hash);
+void mt_hash_free(MtHash* hash);
+
+#endif // MT_HASH_H
